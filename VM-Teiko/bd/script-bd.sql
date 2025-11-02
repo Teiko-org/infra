2999c17755dc62297cad587e5c946a8bce507843
PajorVi <vinicius.marques@sptech.school>
2025-11-02 17:18:42 -0300

    fix UTF

--- VM-Teiko/bd/script-bd.sql
+++ VM-Teiko/bd/script-bd.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE IF NOT EXISTS teiko;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS teiko DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE teiko ;
 
 -- -----------------------------------------------------
